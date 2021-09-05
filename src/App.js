31d90ca5364dd4e5b2a1dc7874eb383e99ec9998
nvdinh185 <nvdinh0766777123@gmail.com>
2021-09-05 11:30:16 +0700

    x

--- src/App.js
+++ src/App.js
@@ -42,31 +42,31 @@ function App() {
       }
     }
 
-    // fetchProductList();
+    fetchProductList();
   }, []);
 
   // Handle firebase auth changed
   useEffect(() => {
-    // const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
-    //   if (!user) {
-    //     // user logs out, handle something here
-    //     console.log('User is not logged in');
-    //     return;
-    //   }
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        // user logs out, handle something here
+        console.log('User is not logged in');
+        return;
+      }
 
-    //   // Get me when signed in
-    //   // const action = getMe();
-    //   try {
-    //     const actionResult = await dispatch(getMe());
-    //     const currentUser = unwrapResult(actionResult);
-    //     console.log('Logged in user: ', currentUser);
-    //   } catch (error) {
-    //     console.log('Failed to login ', error.message);
-    //     // show toast error
-    //   }
-    // });
+      // Get me when signed in
+      // const action = getMe();
+      try {
+        const actionResult = await dispatch(getMe());
+        const currentUser = unwrapResult(actionResult);
+        console.log('Logged in user: ', currentUser);
+      } catch (error) {
+        console.log('Failed to login ', error.message);
+        // show toast error
+      }
+    });
 
-    // return () => unregisterAuthObserver();
+    return () => unregisterAuthObserver();
   }, []);
 
   const handleButtonClick = async () => {
